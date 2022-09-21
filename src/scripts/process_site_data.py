c30a17f0118e190ebfe91070053a6a770d591c0c
Clare72 <cp390@cam.ac.uk>
2022-09-21 17:29:42 +0100

    reformat dataset tsv

--- src/scripts/process_site_data.py
+++ src/scripts/process_site_data.py
@@ -1,12 +1,15 @@
 import pandas as pd
-import re
 
+# new sites must be added to VFB KB, then add FB name and VFB short form here:
 sites_dict= {'EMBL-EBI Single Cell Expression Atlas Datasets': 'scExpressionAtlas'}
 
 dataset_data = pd.read_csv('tmp/dataset_data.tsv', sep='\t')
 
 dataset_data['site'] = dataset_data['site_label'].apply(lambda x: "vfb:" + sites_dict[x])
 dataset_data = dataset_data.drop(['source_linkout', 'site_label'], axis=1).drop_duplicates()
+
+dataset_data = (dataset_data.groupby(['@type', 'id', 'name', 'title', 'publication', 'assay_type', 'site'])\
+  .agg({'accession': lambda x: "|".join(x)}).reset_index())
 
 dataset_data.to_csv('tmp/dataset_data.tsv', sep='\t', index=False)
 
