191bb864327f75ffbf5e5247bb6ee914c9003233
Clare72 <cp390@cam.ac.uk>
2023-11-24 14:15:14 +0000

    change seed method

--- src/scripts/get_gene_list.py
+++ src/scripts/get_gene_list.py
@@ -1,10 +1,12 @@
 import pandas as pd
-from  get_external_terms import get_unique_terms_from_col
-
 
 expression_data = pd.read_csv('tmp/raw_expression_data.tsv', sep='\t')
 
 outfile = "reports/FBgn_list.txt"
+
+def get_unique_terms_from_col(column):
+    """Input is a pandas dataframe column, output is a unique list of values (excluding nulls)."""
+    return list(column.dropna().unique())
 
 FBgns = sorted([g.replace('FlyBase:', '') for g in get_unique_terms_from_col(expression_data['gene'])])
 
