00c6fe465d18aeeac12760e2911a6fb3ada3a556
Clare72 <cp390@cam.ac.uk>
2023-12-21 12:35:08 +0000

    major refactor of exclusions script

--- src/scripts/excluded_datasets_and_assays.py
+++ src/scripts/excluded_datasets_and_assays.py
@@ -1,16 +1,56 @@
 import pandas as pd
 from vfb_connect.neo.neo4j_tools import Neo4jConnect, dict_cursor
 
-# read data files
-cluster_data = pd.read_csv('tmp/raw_cluster_data.tsv', sep='\t')
-dataset_data = pd.read_csv('tmp/raw_dataset_data.tsv', sep='\t')
-sample_data = pd.read_csv('tmp/raw_sample_data.tsv', sep='\t')
-assay_data = pd.read_csv('tmp/raw_assay_data.tsv', sep='\t')
 
-# Manual exclusions - add IDs of datasets to exclude in format "FlyBase:FBlc0000000"
+## input and output
+class DataEntity:
+    name = ''
+    input_file = ''
+    output_file = ''
+    dataframe = None
+    def __init__(self, name):
+        self.name = name
+        self.input_file = 'tmp/raw_%s_data.tsv' % self.name
+        self.output_file = 'tmp/filtered_%s_data.tsv' % self.name
+        self.dataframe = pd.read_csv(self.input_file, sep='\t')
+    def write_tsv(self):
+        self.dataframe.to_csv(self.output_file, sep='\t', index=None)
+    def filter_by_other(self, owncol, other, othercol):
+        """Keep only rows of self.dataframe where owncol (column name) value appears in other.dataframe[othercol]"""
+        self.dataframe = self.dataframe[self.dataframe[owncol].isin(other.dataframe[othercol].drop_duplicates())]
+
+assay_data = DataEntity(name='assay')
+sample_data = DataEntity(name='sample')
+dataset_data = DataEntity(name='dataset')
+clustering_data = DataEntity(name='clustering')
+cluster_data = DataEntity(name='cluster')
+
+
+## Manual exclusions - add IDs of datasets to exclude in format "FlyBase:FBlc0000000"
 manual_exclusions = []
-manually_excluded_datasets = dataset_data[dataset_data['id'].isin(manual_exclusions)][['id', 'name']]
+dataset_data.dataframe = dataset_data.dataframe[~dataset_data.dataframe['id'].isin(manual_exclusions)]
 
+
+## link assays and samples to the same dataset as any clusterings they appear in
+# (this may mean they end up linked to > 1 dataset)
+def update_dataset_from_clustering(input_df):
+    updated_entity_df_list = []
+    for a in input_df['id'].drop_duplicates():
+        single_entity_data = input_df[input_df['id']==a]
+        clustering_datasets = clustering_data.dataframe[clustering_data.dataframe['associated_sample_or_assay_for_clustering']==a]['associated_dataset'].drop_duplicates()
+        updated_dataset_df_list = []
+        for ds in clustering_datasets:
+            updated_dataset = single_entity_data
+            updated_dataset['associated_dataset']=ds
+            updated_dataset_df_list.append(updated_dataset)
+        updated_entity_df_list.append(pd.concat(updated_dataset_df_list))
+    return pd.concat(updated_entity_df_list)
+
+assay_data.dataframe = update_dataset_from_clustering(assay_data.dataframe)
+sample_data.dataframe = update_dataset_from_clustering(sample_data.dataframe)
+
+
+## filter for nervous system term-containing clusters
 # get :Nervous_system FBbt IDs from VFB
 nc = Neo4jConnect('http://pdb.virtualflybrain.org', 'vfb', 'neo4j')
 query = "MATCH (c:Class:Nervous_system) WHERE c.short_form STARTS WITH \"FBbt\" RETURN DISTINCT c.short_form AS FBbt_ID"
@@ -18,26 +58,49 @@ q = nc.commit_list([query])
 FBbt_IDs = pd.DataFrame(dict_cursor(q))
 FBbt_IDs = list(FBbt_IDs['FBbt_ID'].apply(lambda x: x.replace('_', ':')))
 
-# check which datasets not linked to :Nervous_system FBbt terms
-included_cluster_data = cluster_data[cluster_data['cell_type'].isin(FBbt_IDs)]
-non_neuronal_datasets = dataset_data[~dataset_data['id'].isin(included_cluster_data['associated_dataset'])][['id', 'name']].drop_duplicates()
+# check which clusters are linked to :Nervous_system FBbt terms
+ns_cluster_data = cluster_data.dataframe[cluster_data.dataframe['cell_type'].isin(FBbt_IDs)]
+# find datasets that are associated with nervous system clusters
+ns_datasets = ns_cluster_data['associated_dataset'].drop_duplicates()
+# filter datasets
+dataset_data.dataframe = dataset_data.dataframe[dataset_data.dataframe['id'].isin(ns_datasets)]
 
-# assays to exclude based on relationship to a control assay by biological_reference_is
-excluded_assays = assay_data[assay_data['control_assay'].notnull()][['id', 'name']].drop_duplicates()
+# keep only assay, sample, cluster and clustering rows that are associated with a kept dataset
+for d in [cluster_data, clustering_data, assay_data, sample_data]:
+    d.filter_by_other(owncol='associated_dataset', other = dataset_data, othercol = 'id')
 
-# exclude datasets that would have no other assays after removing excluded assays
-exc_dataset_list = []
-for d in dataset_data['id'].unique().tolist():
-    if len(assay_data[(assay_data['associated_dataset']==d) & (~assay_data['id'].isin(excluded_assays['id']))]) == 0:
-        exc_dataset_list.append(d)
 
-experimental_condition_datasets = dataset_data[dataset_data['id'].isin(exc_dataset_list)][['id', 'name']]
+## filtering based on experimental condition assays
+# assay ids to exclude based on relationship to a control assay by biological_reference_is
+excluded_assays = assay_data.dataframe[assay_data.dataframe['control_assay'].notnull()]['id'].drop_duplicates()
+# drop any clusterings that use an excluded assay
+excluded_clusters = clustering_data.dataframe[clustering_data.dataframe['associated_sample_or_assay_for_clustering'].isin(excluded_assays)]['id'].drop_duplicates()
+clustering_data.dataframe = clustering_data.dataframe[~clustering_data.dataframe['id'].isin(excluded_clusters)]
 
-all_exclusions = pd.concat([manually_excluded_datasets, non_neuronal_datasets, excluded_assays, experimental_condition_datasets], axis=0).drop_duplicates()
+# no longer need control_assay column
+assay_data.dataframe.drop('control_assay', axis=1, inplace=True)
 
-included_datasets = [i for i in dataset_data['id'].tolist() if not (i in all_exclusions['id'].tolist())]
+# keep only assay and sample rows that are associated with a kept clustering
+for d in [assay_data, sample_data]:
+    d.filter_by_other(owncol='id', other = clustering_data, othercol = 'associated_sample_or_assay_for_clustering')
 
-all_exclusions.to_csv("tmp/excluded_datasets_and_assays.tsv", sep='\t', index=None)
+# keep only dataset rows that are associated with a kept clustering
+dataset_data.filter_by_other(owncol='id', other = clustering_data, othercol = 'associated_dataset')
 
-with open('tmp/included_dataset_list.txt', 'w') as f:
-    f.write('\n'.join(included_datasets))
+# keep only cluster rows that are associated with a kept clustering
+cluster_data.filter_by_other(owncol='associated_clustering', other = clustering_data, othercol = 'id')
+
+
+## output
+# filtered dataframes
+for d in [dataset_data, cluster_data, clustering_data, assay_data, sample_data]:
+    d.write_tsv()
+
+# make a list of (all) inclusions (for process_metadata and process_expression_data)
+# this is different to internal_terms.txt, which is all entities currently incorporated into ontologies
+all_inclusions = []
+for d in [dataset_data, cluster_data, clustering_data, assay_data, sample_data]:
+    all_inclusions.extend(d.dataframe['id'].drop_duplicates().to_list())
+
+with open('tmp/all_inclusions.txt', 'w') as f:
+    f.write('\n'.join(all_inclusions))
