b004b46d082ac64a6d7c67e089c44baa78071fe9
Clare72 <cp390@cam.ac.uk>
2024-09-16 16:35:53 +0100

    add gene counts to clusters and datasets

--- src/scripts/process_metadata.py
+++ src/scripts/process_metadata.py
@@ -1,6 +1,10 @@
 import modin.pandas as pd
+pd.set_option('display.max_columns',100)
 import argparse
 from filter_data import DataEntity
+from process_expression_data import expression_file_loader
+
+expression_cutoff = 0.2
 
 # new sites must be added to VFB KB, then add FB name and VFB short form here:
 sites_dict= {'EMBL-EBI Single Cell Expression Atlas Datasets': 'scExpressionAtlas'}
@@ -53,6 +57,36 @@ publication_data.filtered_df = pd.DataFrame({"id":dataset_data.filtered_df["publ
 # neo labels (not for Clustering)
 for d in [dataset_data, cluster_data, assay_data, sample_data, publication_data]:
     d.filtered_df['neo_label'] = d.datatype
+
+# get numbers of genes in each cluster and dataset
+def get_gene_counts(input_dataframe, cutoff):
+    """Needs input columns ['id', 'gene', 'expression_extent'] (at least).
+       Output has id as index and columns = ['all_gene_counts', 'filtered_gene_counts']"""
+    unfiltered_genes = input_dataframe.loc[:,['id', 'gene']].drop_duplicates()
+    filtered_genes = input_dataframe.loc[input_dataframe['expression_extent'] > cutoff,['id', 'gene']].drop_duplicates()
+    all_gene_counts = unfiltered_genes.id.value_counts().to_frame(name='all_gene_counts')
+    filtered_gene_counts = filtered_genes.id.value_counts().to_frame(name='filtered_gene_counts')
+    gene_counts = all_gene_counts.join(filtered_gene_counts, how='outer')
+    return gene_counts
+
+# clusters
+cluster_ids = cluster_data.filtered_df['id'].drop_duplicates().to_list()
+exp_data = expression_file_loader(cluster_ids, 0)
+
+cluster_gene_counts = get_gene_counts(exp_data, expression_cutoff)
+cluster_data.filtered_df = cluster_data.filtered_df.join(cluster_gene_counts, on='id')
+
+# datasets
+exp_data = exp_data.merge(cluster_data.filtered_df[['id', 'associated_dataset']], how='left', on='id')
+exp_data['id'] = exp_data['associated_dataset'].astype('category')
+exp_data = exp_data.drop('associated_dataset', axis=1)
+
+dataset_gene_counts = get_gene_counts(exp_data, expression_cutoff)
+dataset_data.filtered_df = dataset_data.filtered_df.join(dataset_gene_counts, on='id')
+
+# if a cluster or dataset has no genes after filtering, drop it (not actually expected to happen)
+cluster_data.filtered_df = cluster_data.filtered_df[cluster_data.filtered_df['filtered_gene_counts'].notnull()]
+dataset_data.filtered_df = dataset_data.filtered_df[dataset_data.filtered_df['filtered_gene_counts'].notnull()]
 
 
 ## spilt data and make new tsvs
